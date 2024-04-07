adeecf16affde29665fde55d313b451ec9578882
Hisashi Yashiro <yashiro.hisashi@nies.go.jp>
2024-04-07 18:25:17 +0900

    Fix cday (for date format of MS Excel)

--- src/n97fit/date/cday.py
+++ src/n97fit/date/cday.py
@@ -4,8 +4,14 @@ from datetime import date, timedelta
 import pandas as pd
 
 # start date
-ys = 1904
+ys = 1900
 sdate = date(ys, 1, 1)
+"""
+Memo:
+    cday is a serial day for MS Excel date format.
+    1900 is a leap year in Excel, but not in Python.
+    That's why the offset is 2.
+"""
 
 # cache dictionaries
 cday_dict = dict()
@@ -13,7 +19,7 @@ date_dict = dict()
 
 
 def date2cday(dt: Union[date, pd.Timestamp]) -> int:
-    """Converts a date to a day count from 1904-01-01
+    """Converts a date to a day count from 1900-01-01
 
     Args:
         year (int): year
@@ -29,14 +35,14 @@ def date2cday(dt: Union[date, pd.Timestamp]) -> int:
     if dt in cday_dict:
         return cday_dict[dt]
 
-    cday = dt.toordinal() - sdate.toordinal()
+    cday = dt.toordinal() - sdate.toordinal() + 2
     cday_dict[dt] = cday
 
     return cday
 
 
 def cday2date(cday: int) -> date:
-    """Converts a day count from 1904-01-01 to a date
+    """Converts a day count from 1900-01-01 to a date
 
     Args:
         cday (int): day count
@@ -48,7 +54,7 @@ def cday2date(cday: int) -> date:
     if cday in date_dict:
         return date_dict[cday]
 
-    cdate = sdate + timedelta(days=int(cday))
+    cdate = sdate + timedelta(days=int(cday)) + timedelta(days=-2)
     date_dict[cday] = cdate
 
     return cdate
